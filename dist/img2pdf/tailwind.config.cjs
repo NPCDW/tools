9caeadb34062e05b4df8dcbb46f68455c8bb3d25
NPCDW <chenyongli0520@gmail.com>
2023-02-23 21:53:35 +0800

    新项目

---
+++ dist/img2pdf/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
