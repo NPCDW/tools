7901d1ede4e6a93c9673b79c311855e2674a04ef
cyl14082 <chenyongli0520@gmail.com>
2023-11-21 12:43:38 +0800

    format

---
+++ dist/format/src/components/Format.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="margin: 20px">
+    <el-input v-model="input" type="textarea" :rows="30"/>
+    <el-button type="primary" @click="format">格式化</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import * as prettier from "prettier";
+import parserHtml from 'prettier/plugins/html'
+import {ref} from "vue";
+
+const input = ref("");
+
+async function format() {
+  input.value = await prettier.format(input.value, {
+    parser: "html",
+    plugins: [parserHtml]
+  });
+}
+</script>
+
+<style scoped>
+</style>
