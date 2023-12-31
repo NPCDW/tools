00fa0b9403c78346f069524f4aa9a0742f17e53f
NPCDW <chenyongli0520@gmail.com>
2023-12-31 16:40:41 +0800

    event stream

---
+++ dist/event-stream/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AiEventStream from './components/AiEventStream.vue'
+</script>
+
+<template>
+  <AiEventStream />
+</template>
+
+<style scoped>
+</style>
---
+++ dist/event-stream/src/components/AiEventStream.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {fetchEventSource} from "@microsoft/fetch-event-source";
+
+fetchEventSource('/api/event-stream', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'text/event-stream'
+  },
+  body: JSON.stringify({
+    event: 'hello',
+  }),
+  openWhenHidden: true,
+  onmessage: (msg) => {
+    console.log(msg.data)
+  },
+  onclose: () => {
+    console.log('connection closed')
+  },
+  onerror: (err) => {
+    console.log(err)
+  },
+  onopen: (response) => {
+    console.log('connection opened', response)
+    return Promise.resolve()
+  }
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
